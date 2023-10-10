cb7315d0270ae460772d72a1619d203bdf140de1
user_name <email_id>
2023-10-10 20:18:05 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 /* Your Code Here */
-
+function createEmployeeRecord(arr) {
+    return {
+      firstName: arr[0],
+      familyName: arr[1],
+      title: arr[2],
+      payPerHour: arr[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  function createEmployeeRecords(arrays) {
+    return arrays.map(createEmployeeRecord);
+  }
+  function createTimeInEvent(employee, dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    employee.timeInEvents.push({ type: "TimeIn", hour: parseInt(hour), date });
+    return employee;
+  }
+  function createTimeOutEvent(employee, dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    employee.timeOutEvents.push({ type: "TimeOut", hour: parseInt(hour), date });
+    return employee;
+  }
+  function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find((event) => event.date === date);
+    const timeOut = employee.timeOutEvents.find((event) => event.date === date);
+    const hoursWorked = (timeOut.hour - timeIn.hour) / 100;
+    return hoursWorked;
+  }
+  function allWagesFor(employee) {
+    const dates = employee.timeInEvents.map((event) => event.date);
+    const totalPay = dates.reduce((acc, date) => {
+      return acc + wagesEarnedOnDate(employee, date);
+    }, 0); // Initialize accumulator to 0
+  
+    return totalPay;
+  }
+  
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
